b7265a50243b3234782d82b823bbe1ce60d58d79
thomasfahy <thomasfahy11@gmail.com>
2024-11-19 19:11:08 +0000

    feat: pushing new webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: 'assets/[name][hash][ext]', // Output to assets folder
+        },
       },
     ],
   },
